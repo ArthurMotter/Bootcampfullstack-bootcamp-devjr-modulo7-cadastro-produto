6babefb3e43e5c34ffff3b53071c0644836ab9e6
Arthur Motter <arthur.motter@fatec.sp.gov.br>
2025-07-21 14:50:39 -0300

    project integration complete

--- product-backend/src/main/java/com/abutua/product_backend/resources/CategoryController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,15 @@ import org.springframework.web.server.ResponseStatusException;
 import com.abutua.product_backend.models.Category;
 
 @RestController
+@CrossOrigin
 public class CategoryController {
 
     /* Categories Instances */
     private List<Category> categories = Arrays.asList(
             new Category(1, "Produção Própria"),
             new Category(2, "Nacional"),
-            new Category(3, "Importado"));
+            new Category(3, "Importado"),
+            new Category(4, "Premium"));
 
     /* Return an specific category by its ID */
     @GetMapping("categories/{id}")
--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -1,25 +1,43 @@
 package com.abutua.product_backend.resources;
 
-import java.util.Arrays;
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.product_backend.models.Product;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin
 public class ProductController {
 
     /* Products Instances */
-    private List<Product> products = Arrays.asList(
-            new Product(1, "Product 01", "Description 01", 1, false, false, 100.50),
-            new Product(2, "Product 02", "Description 02", 2, true, true, 200.50),
-            new Product(3, "Product 03", "Description 03", 1, false, true, 300.50));
+    private List<Product> products = new ArrayList<>();
+
+    /* Save an specific product by request */
+    @PostMapping("products")
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        product.setId(products.size() + 1);
+        products.add(product);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(product.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(product);
+    }
 
     /* Return an specific product by its ID */
     @GetMapping("products/{id}")
